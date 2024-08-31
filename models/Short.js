e43fa6343c407856508c33cde8cd97f921867e56
priya <priyankatathe4@gmail.com>
2024-08-31 11:55:28 +0530

    models update

--- models/Short.js
+++ models/Short.js
@@ -4,7 +4,7 @@ const shortSchema = new mongoose.Schema({
     task: { type: String, required: true },
     desc: { type: String, required: true },
     hero: { type: String, required: true },
-    complete: { type: String, default: true },
+    complete: { type: Boolean, default: true },
 }, { timestamps: true })
 
 module.exports = mongoose.model("short", shortSchema)
